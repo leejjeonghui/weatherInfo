580eb2d93dd39401bec042ba1442a23127fabdbe
leejjeonghui <gdjh0513@naver.com>
2024-08-09 12:13:06 +0900

    initial commit

---
+++ weather/src/test/java/ballpark/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package ballpark.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
